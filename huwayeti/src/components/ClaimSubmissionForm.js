592aafd869f0ccc3396bdcdebb37333a64b5c2a2
Alan Liu <alanl.liu@yahoo.com>
2018-04-28 17:26:54 +0400

    made form look prettier :)

--- huwayeti/src/components/ClaimSubmissionForm.js
+++ huwayeti/src/components/ClaimSubmissionForm.js
@@ -25,14 +25,32 @@ class ClaimSubmissionForm extends React.Component {
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
             <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.11.8/semantic.min.js"></script> 
         </div>
+        <div className="ui raised very padded text container segment">
+        <h2> Add Information </h2>
       <LocalForm className="ui form" onSubmit={(val) => this.handleSubmit(val)}>
+        <div className='field'>
         <Control.text placeholder="Verifier ID" model=".Verifier ID" />
+        </div>
+        <div className='field'>
         <Control.text placeholder="Claimer ID" model=".Claimer ID" />
-        <Control.text placeholder="Type" model=".Type" /> 
-        <Control.text placeholder="Description" model=".Description" />
+        </div>
+        <div className='field'>
+        <Control.select className='ui fluid dropdown' placeholder="Type" model=".Type" > 
+             <option value="">Information Type</option>
+          <option value="med-report">Medical Report</option>
+          <option value="gov-doc">Government Document</option>
+          <option value="skill-certif">Skill Certification</option>
+          </Control.select>
+          </div>
+          <div className='field'>
         <Control.text placeholder="Location" model=".Location" />
+        </div>
+        <div className='field'>
+        <Control.textarea placeholder="Description" model=".Description" />
+        </div>
         <center> <button className="ui button" type="submit">Submit!</button> </center>
       </LocalForm>
+      </div>
       </div>
     );
   }
