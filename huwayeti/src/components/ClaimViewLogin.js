84067f611fe25728053a3f0a99c2b400318ace7f
Sourabh <sourabhlal@gmail.com>
2018-04-28 14:18:28 +0200

    Added claim view auth page

---
+++ huwayeti/src/components/ClaimViewLogin.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class ClaimViewLogin extends React.Component {
+    render() {
+        return (
+            <div>Login yo! :)</div>
+        );
+    }
+}
+
+export default ClaimViewLogin
